3205a6aee7bcf8e1e81ebd223ec668b806959c76
Huda Aeda <hudaaeda10@gmail.com>
2021-03-07 21:04:50 +0700

    Pertemuan #5 - Pagination

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,1 +1,12 @@
-<h1>Hello, <?= $nama; ?>!</h1>
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-md-12">
+            <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                    <h1 class="display-4">Halaman Utama</h1>
+                    <p class="lead">Berikut halaman utama pada aplikasi CI Pertama.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ application/views/peoples/index.php
@@ -0,0 +1,32 @@
+<div class="container">
+    <div class="row mt-3">
+        <div class="col-md-10">
+            <h3>Data People</h3>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($peoples as $people) : ?>
+                        <tr>
+                            <td><?= ++$start; ?></td>
+                            <td><?= $people['name']; ?></td>
+                            <td><?= $people['email']; ?></td>
+                            <td>
+                                <a href="" class="badge badge-primary">Detail</a>
+                                <a href="" class="badge badge-warning">Ubah</a>
+                                <a href="" class="badge badge-danger">hapus</a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+            <?= $this->pagination->create_links(); ?>
+        </div>
+    </div>
+</div>
